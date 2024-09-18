bf3a083d2fe0bea3a9bec62812086c4975e8cbd7
Darian Huotari <darhuotari1@gmail.com>
2024-09-18 09:42:03 -0500

    convert button to form; add try in async function

--- app/index.js
+++ app/index.js
@@ -1,24 +1,40 @@
 // Define the API URL
-//const apiUrl = 'http://localhost:8080/generate?pass_length={pass_length}';
-const apiUrl = 'http://localhost:8080/generate?pass_length=20';
-const passwordField = document.getElementById("password");
+const apiBaseUrl = 'http://localhost:8080/generate?';
 
+// Define the password field as a variable, coming from the input in the form
+const passwordField = document.getElementById("formPassword");
+
+// Define the form so we can take over the submission event
+const form = document.querySelector("#pwdForm")
+
+// Fetch password function
 async function fetchPassword() {
-    // Make a GET request
-    return await fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        return data["generated_password"];
-    })
-    .catch(error => {
-        alert('Error:', error);
-    });
+    
+    // Pull pwdLength from form
+    let pwdLength = document.getElementById("pwdLength").value;
 
+    // Create a valid URL to fetch
+    let apiFullUrl = `${apiBaseUrl}pass_length=${pwdLength}`
+
+    // Make a GET request; return errors to the user
+    try {
+        const response = await fetch(apiFullUrl);
+
+        // If the OK property returns false, throw an error
+        if (!response.ok) {
+          const text = await response.text();
+          throw Error(text);
+        }
+        
+        // Build and return a successful response from the API
+        const jsonResponse = await response.json();
+        return jsonResponse["generated_password"]
+          
+        // Caught errors print to the log and are returned to the user
+      } catch (error) {
+        console.log(error);
+        return (error.message) // would be nice to use something like this: ["detail"]) - we should also style this differently
+      }
 }
 
 function displayPassword(password) {
@@ -29,6 +45,13 @@ async function fetchAndDisplayPassword() {
     const password = await fetchPassword();
     displayPassword(password);
 }
+
+
+// Take over form submission; see https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript#associating_a_formdata_object_and_a_form
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetchAndDisplayPassword();
+});
 
 // We can load this function here to generate a password on page load, if we want
 //fetchAndDisplayPassword();
